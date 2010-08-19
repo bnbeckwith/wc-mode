eb0b23e0de8bcf21c61c1edacd9fe89b2e6888d0
Benjamin Beckwith <bnbeckwith@gmail.com>
2010-08-19 15:47:52 -0400

    Updated documentation to have the proper links to the 750words.com site

--- wc-mode.el
+++ wc-mode.el
@@ -16,6 +16,7 @@
 ;; http://www.neverfriday.com/sweetfriday/2008/06/emacs-tip-word-counting-with-a.html
 ;; The following site had a good idea on how to produce number of chars
 ;; http://xahlee.org/emacs/elisp_count-region.html
+;; Inspired by http://750words.com
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
